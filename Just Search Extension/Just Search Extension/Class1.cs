d41e72c05b2773ee497e4e2d427f6daf55950680
Tony-Chau <tonychau923@gmail.com>
2019-03-31 21:38:25 +1000

    Made a custom activity folder for this program

---
+++ Just Search Extension/Just Search Extension/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Just_Search_Extension
+{
+
+
+    public class OpenBrowser : CodeActivity
+    {
+        [Category("Input")]
+        public InArgument<string> query { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string q = query.Get(context);
+            ProcessStartInfo web = new ProcessStartInfo("www.google.com/search?" + q);
+            Process.Start(web);
+        }
+    }
+}
