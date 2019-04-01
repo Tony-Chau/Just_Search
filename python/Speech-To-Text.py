f4ec80dec6ac0f467d44a97ebb0e8913a897d522
Tony-Chau <tonychau923@gmail.com>
2019-04-01 19:35:48 +1000

    Made some more update

---
+++ python/Speech-To-Text.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+import pyaudio
+
+def SpeechToText():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        return r.recognize_google(audio)
+    except:
+        return ""
