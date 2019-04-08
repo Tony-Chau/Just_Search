3c81062f13e3642bbf1ce4dd550290b1ec44ffca
Tony-Chau <tonychau923@gmail.com>
2019-04-08 10:39:52 +1000

    Added new solution to test some stuff

---
+++ Just Search Extension/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = " green computer search computer sd search green";
+            int index = word.IndexOf("computer search") + 16;
+            int length = word.Length - index;
+            Console.WriteLine(word.Substring(index, length));
+            Console.ReadKey();
+        }
+    }
+}
